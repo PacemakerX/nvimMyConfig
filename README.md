628536598f5504353d79da84268eb6956b463af1
Sparsh Soni <sparsh.officialwork@gmail.com>
2025-04-25 14:34:29 +0000

    my changes

---
+++ README.md
@@ -0,0 +1,15 @@
+# Vim-Nvim
+My neovim config files.
+>Plugin Manager used: [packer.nvim](https://github.com/wbthomason/packer.nvim)
+---
+
+### Screenshots
+
+![Wednesday 10 July 2024 09:35:40 PM IST](https://github.com/Ashwani1330/Vim-Nvim/assets/84962178/c46c3426-c027-451c-942f-2c00ba4789b6)
+
+![Wednesday 10 July 2024 09:37:36 PM IST](https://github.com/Ashwani1330/Vim-Nvim/assets/84962178/c2785a08-5533-4b60-ab14-72750e8df61b)
+
+---
+Highly inspired by ThePrimeagen neovim config availabe at -> https://github.com/ThePrimeagen/init.lua
+
+Enjoy!
---
+++ after/plugin/lsp.lua
@@ -0,0 +1,86 @@
+local lsp_zero = require('lsp-zero')
+
+lsp_zero.extend_lspconfig()
+
+-- Mason setup
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = {
+    'lua_ls',
+    'clangd',
+    -- 'rust_analyzer', -- Commented out as per original config
+  },
+  handlers = {
+    lsp_zero.default_setup,
+    -- Custom handler to exclude rust_analyzer
+    function(server_name)
+      if server_name ~= "rust_analyzer" then
+        require('lspconfig')[server_name].setup({})
+      end
+    end,
+  }
+})
+
+-- Configure completion
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-Space>'] = cmp.mapping.complete(),
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- Configure diagnostics
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  up1te_in_insert = false,
+  underline = true,
+  severity_sort = true,
+  float = {
+    focusable = false,
+    style = "minimal",
+    border = "rounded",
+    source = "always",
+    header = "",
+    prefix = "",
+  },
+})
+
+-- Set up custom signs
+local signs = { Error = "✘", Warn = "▲", Hint = "⚑", Info = "" }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
+-- Set up keymaps
+lsp_zero.on_attach(function(client, bufnr)
+  local opts = {buffer = bufnr, remap = false}
+  
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+end)
